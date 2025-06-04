c46a1d8820b4047fa4ad47d5093914383b699846
Doan Huu Quang <doanhuuquang.dev@gmail.com>
2025-06-04 15:54:15 +0700

    Edit not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function NotFound() {
-  return <h1>404 - Page Not Found hhehehe</h1>;
+  return (
+    <div className="w-[100vw] h-[100vh] bg-background dark:bg-sidebar flex flex-col items-center justify-center gap-3">
+      <Image
+        alt="404"
+        src={"assets/images/404-error-with-a-broken-robot.svg"}
+        width={400}
+        height={100}
+      />
+      <h2 className="text-muted-foreground">
+        Không tìm thấy trang bạn yêu cầu
+      </h2>
+      <Link href={"/"} className="px-10 py-2 rounded-md bg-accent">
+        Quay về trang chủ
+      </Link>
+    </div>
+  );
 }
