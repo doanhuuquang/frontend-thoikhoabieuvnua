9f26541c86f425cee92313a88e1c3f4450994aeb
Doan Huu Quang <doanhuuquang.dev@gmail.com>
2025-06-05 19:17:41 +0700

    Chỉnh sửa hiệu ứng loading

--- src/components/shared/loading.tsx
+++ src/components/shared/loading.tsx
@@ -44,7 +44,7 @@ function LoadingContent({
   return (
     <div
       className={cn(
-        "z-51 fixed t-[50%] left-[50%] -translate-x-[50%] translate-y-[50%] flex flex-col items-center gap-10",
+        "z-51 fixed t-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] flex flex-col items-center gap-10",
         className
       )}
       {...props}
